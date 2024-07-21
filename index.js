bfe7b2bd7777420926a6ad629788937862911fe7
pataibalazs <79509500+pataibalazs@users.noreply.github.com>
2024-07-21 16:45:14 +0200

    added prodval

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ const path = require("path");
 
 const PORT = process.env.PORT || 5001;
 
-express()
+const app = express();
+
+app
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
   .get("/cool", (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .get("/product-validation", (req, res) => {
+    const products = [
+      {
+        id: "metin-gold",
+        name: "METIN2 ARANY",
+        price: 12.11,
+        customFields: [],
+        url: "https://thawing-dawn-87843-f5b692533558.herokuapp.com/product-validation",
+      },
+    ];
+    res.json(products);
+  });
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
