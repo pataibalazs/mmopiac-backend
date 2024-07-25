23dc537f24416f4020509aa0a6c3e956768fcf18
pataibalazs <79509500+pataibalazs@users.noreply.github.com>
2024-07-25 12:17:23 +0200

    asd

--- index.js
+++ index.js
@@ -65,8 +65,4 @@ app
     res.status(200).send("Webhook received successfully");
   });
 
-http.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
-
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
